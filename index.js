f6eba12ecb5c4db4b9112271f5a178d92661d09f
ivansanguezax <ivansanguezax@gmail.com>
2023-09-19 21:26:42 -0400

    routing

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ const app = express();
 // Define una ruta GET para el endpoint raíz '/'
 app.get('/', (req, res) =>{
     // Envía el archivo 'index.html' cuando se accede a la ruta raíz
-    res.sendFile('./static/index.html', {
-        root: __dirname
-    });
+    // res.sendFile('./static/index.html', {
+    //     root: __dirname
+    // });
+
+    res.send('Hello World');
+})
+
+//Routing
+app.get('/about', (req, res) =>{
+    res.send('About');
+})
+
+//Routing
+app.get('/weather', (req, res) =>{
+
+    res.send('the current weather is 50 degrees');
+})
+
+app.use((req, res) =>{
+    res.status(404).send('No encontramos la página');
 })
 
 // Escucha las solicitudes en el puerto 3000
-app.listen('3000');
+app.listen('2300');
 
 // Imprime un mensaje en la consola indicando que el servidor está escuchando en el puerto 3000
-console.log(`Server at port ${3000}`);
+console.log(`Server at port ${2300}`);
