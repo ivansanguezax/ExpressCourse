29c1a35f3bc37eb2381d152db771d79b00bf9334
ivansanguezax <ivansanguezax@gmail.com>
2023-09-23 23:18:25 -0400

    EJS synax

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,8 +3,31 @@ const {Router} = require('express');
 const router = Router()
 
 router.get('/', (req, res) =>{
+    let isActive = true;
+
+    const users = [
+        {
+            id: 1,
+            name: 'Jhon',
+            age: 25
+        },
+        {
+            id: 2,
+            name: 'Jane',
+            age: 30
+        },
+        {
+            id: 3,
+            name: 'Mark',
+            age: 15
+        }
+    ];
+
     res.render('index',{
-        title : 'Mundo'})
+        title : 'Mundo',
+        isActive : isActive,
+        users : users
+    })
 });
 
 router.get('/about', (req, res) =>{
